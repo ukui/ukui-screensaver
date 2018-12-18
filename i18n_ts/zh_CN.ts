1d36f5ec3756f001831c675f5d1de7b2cd7a082f
yanghao <yanghao@kylinos.cn>
2018-12-18 16:46:41 +0800

    Update Chinese translation.

--- i18n_ts/zh_CN.ts
+++ i18n_ts/zh_CN.ts
@@ -4,130 +4,169 @@
 <context>
     <name>AuthDialog</name>
     <message>
-        <location filename="../src/authdialog.ui" line="14"/>
+        <location filename="../build/src/ui_authdialog.h" line="191"/>
         <source>Form</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/authdialog.ui" line="62"/>
+        <location filename="../build/src/ui_authdialog.h" line="192"/>
         <source>More Devices</source>
         <translation>选择其他设备</translation>
     </message>
     <message>
-        <location filename="../src/authdialog.ui" line="84"/>
+        <location filename="../build/src/ui_authdialog.h" line="193"/>
         <source>Biometric</source>
         <translation>使用生物识别认证</translation>
     </message>
     <message>
-        <location filename="../src/authdialog.ui" line="106"/>
+        <location filename="../build/src/ui_authdialog.h" line="194"/>
         <source>Password</source>
         <translation>使用密码认证</translation>
     </message>
     <message>
-        <location filename="../src/authdialog.ui" line="134"/>
+        <location filename="../build/src/ui_authdialog.h" line="195"/>
+        <location filename="../src/authdialog.cpp" line="396"/>
         <source>Retry</source>
         <translation>重试</translation>
     </message>
     <message>
-        <location filename="../src/authdialog.ui" line="315"/>
+        <location filename="../build/src/ui_authdialog.h" line="199"/>
         <source>UnLock</source>
         <translation>解锁</translation>
     </message>
     <message>
-        <location filename="../src/authdialog.ui" line="426"/>
+        <location filename="../build/src/ui_authdialog.h" line="204"/>
         <source>LoggedIn</source>
         <translation>已登录</translation>
     </message>
     <message>
-        <location filename="../src/authdialog.cpp" line="190"/>
+        <location filename="../src/authdialog.cpp" line="179"/>
+        <source>Password: </source>
+        <translation>密码：</translation>
+    </message>
+    <message>
+        <location filename="../src/authdialog.cpp" line="194"/>
         <source>Password Incorrect, Please try again</source>
         <translation>密码错误，请重试</translation>
     </message>
-</context>
-<context>
-    <name>BioAuthWidget</name>
     <message>
-        <location filename="../src/BioAuth/src/bioauthwidget.ui" line="14"/>
-        <source>Form</source>
-        <translation></translation>
+        <location filename="../src/authdialog.cpp" line="362"/>
+        <source>Biometric Authentication</source>
+        <translation>生物识别认证</translation>
     </message>
     <message>
-        <location filename="../src/BioAuth/src/bioauthwidget.ui" line="49"/>
-        <location filename="../src/BioAuth/src/bioauthwidget.ui" line="90"/>
-        <source>TextLabel</source>
-        <translation></translation>
+        <location filename="../src/authdialog.cpp" line="374"/>
+        <source>Password Authentication</source>
+        <translation>密码认证</translation>
     </message>
     <message>
-        <location filename="../src/BioAuth/src/bioauthwidget.ui" line="127"/>
-        <source>More</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../src/BioAuth/src/bioauthwidget.ui" line="140"/>
-        <source>Retry</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../src/BioAuth/src/bioauthwidget.ui" line="153"/>
-        <source>Password</source>
-        <translation></translation>
+        <location filename="../src/authdialog.cpp" line="386"/>
+        <source>Other Devices</source>
+        <translation>其他设备</translation>
     </message>
 </context>
 <context>
     <name>BioDevices</name>
     <message>
-        <location filename="../src/BioAuth/src/biodevices.cpp" line="170"/>
         <source>FingerPrint</source>
-        <translation>指纹</translation>
+        <translation type="vanished">指纹</translation>
     </message>
     <message>
-        <location filename="../src/BioAuth/src/biodevices.cpp" line="172"/>
         <source>FingerVein</source>
-        <translation>指静脉</translation>
+        <translation type="vanished">指静脉</translation>
     </message>
     <message>
-        <location filename="../src/BioAuth/src/biodevices.cpp" line="174"/>
         <source>Iris</source>
-        <translation>虹膜</translation>
+        <translation type="vanished">虹膜</translation>
     </message>
     <message>
-        <location filename="../src/BioAuth/src/biodevices.cpp" line="176"/>
         <source>Face</source>
-        <translation>人脸</translation>
+        <translation type="vanished">人脸</translation>
     </message>
     <message>
-        <location filename="../src/BioAuth/src/biodevices.cpp" line="178"/>
         <source>VoicePrint</source>
-        <translation>声纹</translation>
+        <translation type="vanished">声纹</translation>
     </message>
 </context>
 <context>
     <name>BioDevicesWidget</name>
     <message>
-        <location filename="../src/BioAuth/src/biodeviceswidget.ui" line="14"/>
-        <source>Form</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../src/BioAuth/src/biodeviceswidget.ui" line="31"/>
         <source>Please select other biometric devices</source>
-        <translation>请选择其他生物识别设备</translation>
+        <translation type="vanished">请选择其他生物识别设备</translation>
     </message>
     <message>
-        <location filename="../src/BioAuth/src/biodeviceswidget.ui" line="52"/>
         <source>Device Type:</source>
+        <translation type="vanished">设备类型：</translation>
+    </message>
+    <message>
+        <source>Device Name:</source>
+        <translation type="vanished">设备名称：</translation>
+    </message>
+</context>
+<context>
+    <name>BiometricAuthWidget</name>
+    <message>
+        <location filename="../BiometricAuth/biometricauthwidget.cpp" line="66"/>
+        <source>Current device: </source>
+        <translation>当前设备：</translation>
+    </message>
+</context>
+<context>
+    <name>BiometricDevicesWidget</name>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceswidget.cpp" line="29"/>
+        <source>Please select the biometric device</source>
+        <translation>请选择生物设备</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceswidget.cpp" line="34"/>
+        <source>Device type:</source>
         <translation>设备类型：</translation>
     </message>
     <message>
-        <location filename="../src/BioAuth/src/biodeviceswidget.ui" line="70"/>
-        <source>Device Name:</source>
-        <translation>设备名称：</translation>
+        <location filename="../BiometricAuth/biometricdeviceswidget.cpp" line="48"/>
+        <source>Device name:</source>
+        <translation>设备型号：</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceswidget.cpp" line="56"/>
+        <source>OK</source>
+        <translation>确定</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceType</name>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="23"/>
+        <source>FingerPrint</source>
+        <translation>指纹</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="25"/>
+        <source>FingerVein</source>
+        <translation>指静脉</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="27"/>
+        <source>Iris</source>
+        <translation>虹膜</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="29"/>
+        <source>Face</source>
+        <translation>人脸</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="31"/>
+        <source>VoicePrint</source>
+        <translation>声纹</translation>
     </message>
 </context>
 <context>
     <name>KeyboardWidget</name>
     <message>
-        <location filename="../src/VirtualKeyboard/src/keyboardwidget.ui" line="29"/>
+        <location filename="../VirtualKeyboard/src/keyboardwidget.ui" line="29"/>
+        <location filename="../build/VirtualKeyboard/VirtualKeyboard_autogen/include/ui_keyboardwidget.h" line="790"/>
         <source>KeyboardWidget</source>
         <translation></translation>
     </message>
@@ -136,16 +175,19 @@
     <name>LockWidget</name>
     <message>
         <location filename="../src/lockwidget.ui" line="14"/>
+        <location filename="../build/src/ui_lockwidget.h" line="61"/>
         <source>Form</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/lockwidget.ui" line="51"/>
+        <location filename="../src/lockwidget.ui" line="54"/>
+        <location filename="../build/src/ui_lockwidget.h" line="63"/>
         <source>Date</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/lockwidget.ui" line="64"/>
+        <location filename="../src/lockwidget.ui" line="67"/>
+        <location filename="../build/src/ui_lockwidget.h" line="64"/>
         <source>Time</source>
         <translation></translation>
     </message>
