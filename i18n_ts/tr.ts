751f5d090eca6cf78fe62acd58d2388423c9895c
Aylinux <cihanalk@gmail.com>
2020-05-18 16:32:46 +0300

    Create tr.ts

---
+++ i18n_ts/tr.ts
@@ -0,0 +1,324 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>AuthDialog</name>
+    <message>
+        <source>More Devices</source>
+        <translation type="obsolete">Diğer Cihazlar</translation>
+    </message>
+    <message>
+        <source>Biometric</source>
+        <translation type="obsolete">Biometrik</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation type="obsolete">Parola</translation>
+    </message>
+    <message>
+        <location filename="../src/authdialog.cpp" line="454"/>
+        <source>Retry</source>
+        <translation type="unfinished">Yeniden Dene</translation>
+    </message>
+    <message>
+        <source>UnLock</source>
+        <translation type="obsolete">Kilidi Aç</translation>
+    </message>
+    <message>
+        <source>LoggedIn</source>
+        <translation type="obsolete"></translation>
+    </message>
+    <message>
+        <location filename="../src/authdialog.cpp" line="237"/>
+        <source>Password: </source>
+        <translation type="unfinished">Parola</translation>
+    </message>
+    <message>
+        <source>Account locked %1 minutes due to %2 fail attempts</source>
+        <translation type="vanished">%2 başarısız denemeden dolayı hesap %1 dakika kilitlendi</translation>
+    </message>
+    <message>
+        <location filename="../src/authdialog.cpp" line="253"/>
+        <source>Password Incorrect, Please try again</source>
+        <translation type="unfinished">Parola yanlış, Lütfen tekrar deneyin</translation>
+    </message>
+    <message>
+        <source>Authentication failure,there are still %1 remaining opportunities</source>
+        <translation type="vanished">Kimlik doğrulama hatası, hala %1 kalan denemen var</translation>
+    </message>
+    <message>
+        <location filename="../src/authdialog.cpp" line="419"/>
+        <source>Biometric Authentication</source>
+        <translation type="unfinished">Biyometrik Kimlik Doğrulama</translation>
+    </message>
+    <message>
+        <location filename="../src/authdialog.cpp" line="432"/>
+        <source>Password Authentication</source>
+        <translation type="unfinished">Parola Doğrulama</translation>
+    </message>
+    <message>
+        <location filename="../src/authdialog.cpp" line="444"/>
+        <source>Other Devices</source>
+        <translation type="unfinished">Diğer Ayıtlar</translation>
+    </message>
+</context>
+<context>
+    <name>BioDevices</name>
+    <message>
+        <source>FingerPrint</source>
+        <translation type="obsolete">Parmak İzi</translation>
+    </message>
+    <message>
+        <source>FingerVein</source>
+        <translation type="obsolete">Damar İzi</translation>
+    </message>
+    <message>
+        <source>Iris</source>
+        <translation type="obsolete">Göz</translation>
+    </message>
+    <message>
+        <source>Face</source>
+        <translation type="obsolete">Yüz</translation>
+    </message>
+    <message>
+        <source>VoicePrint</source>
+        <translation type="obsolete">Ses İzi</translation>
+    </message>
+</context>
+<context>
+    <name>BioDevicesWidget</name>
+    <message>
+        <source>Please select other biometric devices</source>
+        <translation type="obsolete">Lütfen diğer biyometrik cihazları seçin</translation>
+    </message>
+    <message>
+        <source>Device Type:</source>
+        <translation type="obsolete">Aygıt Türü:</translation>
+    </message>
+    <message>
+        <source>Device Name:</source>
+        <translation type="obsolete">Aygıt Adı:</translation>
+    </message>
+</context>
+<context>
+    <name>BiometricAuthWidget</name>
+    <message>
+        <location filename="../BiometricAuth/biometricauthwidget.cpp" line="99"/>
+        <source>Current device: </source>
+        <translation type="unfinished">Şuanki aygıt:</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricauthwidget.cpp" line="160"/>
+        <source>Identify failed, Please retry.</source>
+        <translation type="unfinished">Tanımlama başarısız, Lütfen tekrar deneyin.</translation>
+    </message>
+</context>
+<context>
+    <name>BiometricDevicesWidget</name>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceswidget.cpp" line="47"/>
+        <source>Please select the biometric device</source>
+        <translation type="unfinished">Lütfen biyometrik aygıtı seçin</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceswidget.cpp" line="52"/>
+        <source>Device type:</source>
+        <translation type="unfinished">Aygıt türü:</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceswidget.cpp" line="68"/>
+        <source>Device name:</source>
+        <translation type="unfinished">Aygıt adı:</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceswidget.cpp" line="78"/>
+        <source>OK</source>
+        <translation type="unfinished">Tamam</translation>
+    </message>
+</context>
+<context>
+    <name>DeviceType</name>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="41"/>
+        <source>FingerPrint</source>
+        <translation type="unfinished">Parmak İzi</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="43"/>
+        <source>FingerVein</source>
+        <translation type="unfinished">Parmak Damarı</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="45"/>
+        <source>Iris</source>
+        <translation type="unfinished">Göz</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="47"/>
+        <source>Face</source>
+        <translation type="unfinished">Yüz</translation>
+    </message>
+    <message>
+        <location filename="../BiometricAuth/biometricdeviceinfo.cpp" line="49"/>
+        <source>VoicePrint</source>
+        <translation type="unfinished">Ses İzi</translation>
+    </message>
+</context>
+<context>
+    <name>KeyboardWidget</name>
+    <message>
+        <location filename="../VirtualKeyboard/src/keyboardwidget.ui" line="29"/>
+        <source>KeyboardWidget</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LockWidget</name>
+    <message>
+        <location filename="../src/lockwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/lockwidget.ui" line="54"/>
+        <source>Date</source>
+        <translation type="unfinished">Tarih</translation>
+    </message>
+    <message>
+        <location filename="../src/lockwidget.ui" line="67"/>
+        <source>Time</source>
+        <translation type="unfinished">Zaman</translation>
+    </message>
+    <message>
+        <location filename="../src/lockwidget.cpp" line="247"/>
+        <source>Guest</source>
+        <translation type="unfinished">Misafir</translation>
+    </message>
+    <message>
+        <source>SwitchUser</source>
+        <translation type="vanished">Kullanıcı Değiştir</translation>
+    </message>
+</context>
+<context>
+    <name>PowerManager</name>
+    <message>
+        <source>lock</source>
+        <translation type="vanished">kilit</translation>
+    </message>
+    <message>
+        <source>SwitchUser</source>
+        <translation type="vanished">Kullanıcı Değiştir</translation>
+    </message>
+    <message>
+        <source>logout</source>
+        <translation type="vanished">Çıkış</translation>
+    </message>
+    <message>
+        <source>reboot</source>
+        <translation type="vanished">yeniden başlat</translation>
+    </message>
+    <message>
+        <source>shutdown</source>
+        <translation type="vanished">kapat</translation>
+    </message>
+    <message>
+        <location filename="../src/powermanager.cpp" line="109"/>
+        <source>Lock Screen</source>
+        <translation type="unfinished">Ekranı Kilitle</translation>
+    </message>
+    <message>
+        <location filename="../src/powermanager.cpp" line="124"/>
+        <source>Switch User</source>
+        <translation type="unfinished">Kullanıcı Değiştir</translation>
+    </message>
+    <message>
+        <location filename="../src/powermanager.cpp" line="138"/>
+        <source>Log Out</source>
+        <translation type="unfinished">Çıkış</translation>
+    </message>
+    <message>
+        <location filename="../src/powermanager.cpp" line="152"/>
+        <source>Restart</source>
+        <translation type="unfinished">Yeniden Başlat</translation>
+    </message>
+    <message>
+        <location filename="../src/powermanager.cpp" line="166"/>
+        <source>Power Off</source>
+        <translation type="unfinished">Bilgisayarı Kapat</translation>
+    </message>
+</context>
+<context>
+    <name>Screensaver</name>
+    <message>
+        <source>exit(Esc)</source>
+        <translation type="vanished">çıkış(Esc)</translation>
+    </message>
+    <message>
+        <location filename="../screensaver/screensaver.cpp" line="243"/>
+        <source>exit</source>
+        <translation type="unfinished">çıkış</translation>
+    </message>
+    <message>
+        <location filename="../screensaver/screensaver.cpp" line="268"/>
+        <source>Set as desktop wallpaper</source>
+        <translation type="unfinished">Masaüstü arkaplanı olarak ayarla</translation>
+    </message>
+    <message>
+        <location filename="../screensaver/screensaver.cpp" line="277"/>
+        <source>Automatic switching</source>
+        <translation type="unfinished">Otomatik değiştir</translation>
+    </message>
+</context>
+<context>
+    <name>SleepTime</name>
+    <message>
+        <location filename="../screensaver/sleeptime.cpp" line="70"/>
+        <source>You have rested:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/ukui-screensaver-command.cpp" line="30"/>
+        <source>Start command for the ukui ScreenSaver.</source>
+        <translation type="unfinished">Ukui Ekran Koruyucu için başlatma komutu.</translation>
+    </message>
+    <message>
+        <location filename="../src/ukui-screensaver-command.cpp" line="35"/>
+        <location filename="../src/ukui-screensaver-dialog.cpp" line="130"/>
+        <source>lock the screen immediately</source>
+        <translation type="unfinished">Ekranı hemen kilitle</translation>
+    </message>
+    <message>
+        <location filename="../src/ukui-screensaver-dialog.cpp" line="125"/>
+        <source>Dialog for the ukui ScreenSaver.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/ukui-screensaver-dialog.cpp" line="132"/>
+        <source>activated by session idle signal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screensaver/main.cpp" line="55"/>
+        <source>Screensaver for ukui-screensaver</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screensaver/main.cpp" line="59"/>
+        <source>show on root window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screensaver/main.cpp" line="61"/>
+        <source>show on window.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../screensaver/main.cpp" line="62"/>
+        <source>window id</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
