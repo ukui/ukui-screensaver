5e2e727babcd7b77d5aed16cd4c26c65345a6acf
liuyuanpeng <liuyuanpeng@kylinos.cn>
2020-06-29 18:06:44 +0800

    add hdpi

--- screensaver/main.cpp
+++ screensaver/main.cpp
@@ -33,6 +33,10 @@
 
 int main(int argc, char *argv[])
 {
+#if(QT_VERSION>=QT_VERSION_CHECK(5,6,0))
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
     QApplication a(argc, argv);
 	
     //加载翻译文件
