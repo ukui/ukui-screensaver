d99f67007159918d9f8bab7a34fa6299604e44a9
liuyuanpeng <liuyuanpeng@kylinos.cn>
2021-09-02 14:14:49 +0800

    修复人脸显示偏紫色的问题

--- BiometricAuth/biometricauthwidget.cpp
+++ BiometricAuth/biometricauthwidget.cpp
@@ -247,6 +247,7 @@ void BiometricAuthWidget::onFrameWritten(int drvid)
 
     cv::Mat mat2(1, sizeof(base64_bufferData), CV_8U, base64_bufferData);
     img = cv::imdecode(mat2, cv::IMREAD_COLOR);
+    cv::cvtColor(img,img,cv::COLOR_BGR2RGB);
 
     QImage srcQImage = QImage((uchar*)(img.data), img.cols, img.rows, QImage::Format_RGB888);
     lblImage->setFixedSize(160,160);
